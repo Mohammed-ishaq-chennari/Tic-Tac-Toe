5b4a1275be533e21427deae32df10447aef82645
Mohammed Ishaq Chennari <ishaqc145@gmail.com>
2024-01-04 12:40:42 +0530

    gamexpro

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Tictoc from './components/TicTacToe/Tictoc'
+
+
+function App() {
+  return (
+    <div>
+   <Tictoc/>
+    </div>
+  )
+}
+
+export default App
